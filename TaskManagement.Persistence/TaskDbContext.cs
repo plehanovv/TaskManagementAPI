99352ad874e5694e5ade240bdcaad0490bc91ebb
yaroslav <plehanov.kzn2@gmail.com>
2025-03-10 21:04:12 +0300

    Настройка бд миграций

--- TaskManagement.Persistence/TaskDbContext.cs
+++ TaskManagement.Persistence/TaskDbContext.cs
@@ -6,6 +6,16 @@ namespace TaskManagement.Persistence;
 public class TaskDbContext : DbContext
 {
     public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
-    
+
     public DbSet<TaskEntity> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=taskdb.sqlite", 
+                options => options.MigrationsAssembly("TaskManagement.Infrastructure"));
+        }
+    }
 }
